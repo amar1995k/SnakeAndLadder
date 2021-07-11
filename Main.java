84812a547a00e9bb4fce48362c29ee3004047854
amar1995k <67822357+amar1995k@users.noreply.github.com>
2021-07-11 15:23:56 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package snakeAndLadder;
+
+public class Main {
+	public static void main(String[] args) {
+	    int pos = 0;
+        System.out.println("Welcome to Snake And Ladder program");
+        System.out.println("Single player at start position "+ pos);
+        
+    }
+
+}
